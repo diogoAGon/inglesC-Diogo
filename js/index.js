708ffa3433cf3e54e4277239e211798d5316a228
devdiogo1 <dev1diogo@outlook.com>
2023-12-19 13:50:53 -0300

    Adicionando animacoes + buttons na tela principal

---
+++ js/index.js
@@ -0,0 +1,47 @@
+
+
+
+
+// Codigo para o botao aparecer conforme os tempos.
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => { 
+        document.querySelectorAll('.ELT').forEach((element) => {
+            element.style.opacity = '1';
+        });
+    }, 2700);
+    setTimeout(() => { 
+        document.querySelectorAll('.ET').forEach((element) => {
+            element.style.opacity = '1';
+        });
+    }, 2850);
+    setTimeout(() => { 
+        document.querySelectorAll('.ETM').forEach((element) => {
+            element.style.opacity = '1';
+        });
+    }, 2950);
+});
+
+
+
+// TYPING
+document.addEventListener("DOMContentLoaded", () => {
+    const fraseElement = document.querySelector('.frase');
+    const h2Element = fraseElement.querySelector('h2');
+    h2Element.innerHTML = ''; // Limpa o conteúdo original do h2
+
+    const textoOriginal = "Bom dia, pegue o seu cafe e bons estudos!";
+    let index = 0;
+
+    const typeText = () => {
+        h2Element.innerHTML += textoOriginal.charAt(index);
+        index++;
+
+        if (index < textoOriginal.length) {
+            setTimeout(typeText, 50);
+        }
+    };
+
+    typeText();
+});
+
+
